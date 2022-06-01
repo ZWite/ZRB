fb872ae70f78c6dda6b7ef47c6d0adaeb4816a28
张孟鑫 <zhangmengxin@tiamaes.com>
2022-06-01 19:37:26 +0800

    提供一个反射工具，获取字段值，利用二进制运算进行首字母大写

--- ZRB-common/src/main/java/com/zhang/ClassFlies.java
+++ ZRB-common/src/main/java/com/zhang/ClassFlies.java
@@ -1,5 +1,7 @@
 package com.zhang;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,6 +14,7 @@ import java.util.Iterator;
  * @date: 2022/6/1 15:39
  * @version 1.0
  */
+@Slf4j
 public class ClassFlies {
 
     public <T> void getValue(Collection<T> collection){
@@ -33,12 +36,9 @@ public class ClassFlies {
                 try {
                     Method method = aClass.getMethod(s1);
                     invoke = method.invoke(t,new Object[]{});
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    log.error("ClassFlies.getValue方法出现异常"+ e);
+                    //e.printStackTrace();
                 }
                 //其它数据类型都当作字符串简单处理
                 if (invoke != null) {
@@ -60,5 +60,35 @@ public class ClassFlies {
             c^=32;
         }
         return c;
+    }
+
+
+    /**
+     * 单个对象的某个键的值
+     * @param obj 对象
+     * @param key 键
+     * @return Object 键在对象中所对应得值 没有查到时返回空字符串
+     */
+    public static Object getValueByKey(Object obj, String key) {
+        // 得到类对象
+        Class<?> cls = obj.getClass();
+        /* 得到类中的所有属性集合 */
+        Field[] fs = cls.getDeclaredFields();
+        for (int i = 0; i < fs.length; i++) {
+            Field f = fs[i];
+            //设置些属性是可以访问的
+            f.setAccessible(true);
+            try {
+                if (f.getName().endsWith(key)) {
+                    System.out.println("单个对象的某个键的值==反射==" + f.get(obj));
+                    return f.get(obj);
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
     }
 }
